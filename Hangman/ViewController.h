b2432c18bdec002c989458f1959acc56ad14f835
Neil Argade <neilargade@gmail.com>
2015-03-20 11:20:51 -0700

    Initial Commit

---
+++ Hangman/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hangman
+//
+//  Created by Neil Argade on 3/20/15.
+//  Copyright (c) 2015 Neil Argade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Hangman/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Hangman
+//
+//  Created by Neil Argade on 3/20/15.
+//  Copyright (c) 2015 Neil Argade. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
